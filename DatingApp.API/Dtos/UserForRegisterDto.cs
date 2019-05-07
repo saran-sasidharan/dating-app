e706f78b99f1b2444878eab98ab6cf16d644e712
Saran Sasidharan <saran.s.dhar@gmail.com>
2019-05-08 02:40:48 +0530

    1. Added login method in auth controller. Login method returns JWT. Reads key from app settings configuration file. Not the best way to save key. Will be updated later. 2. Validation of UserForRegisterDto properties are added as annotations. 3. Added authentication middleware to validate the token send by user. Also restricted access of getValues method in ValuesController.

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, 
+            ErrorMessage = "Password must be atleast 4 chars and maximum of 8!")]
         public string Password { get; set; }
     }
 }
